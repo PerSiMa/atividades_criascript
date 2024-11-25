b64cebeabe47cb2506613e830d6d9b6c4a0c5eeb
Pedro <alzmololz@gmail.com>
2024-11-25 16:10:00 -0300

    primeiro commit

---
+++ src/app/pedro/page.tsx
@@ -0,0 +1,162 @@
+
+export default function Pedro() {
+  return (
+    <div style={{overflow: 'hidden'}}>
+      
+      <h1 style={{display: 'flex', width: '100vw', justifyContent: 'center'}}>Pedro página exercício display flex</h1>
+
+      {/* Bloco superior das DIVs : "Flex direction row" e "Flex direction row-reverse" */}
+      <div style={{width: '100vw', height: '720px', display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+
+        <div style={{fontWeight: 'bold', fontSize: '30px', display: 'flex', width: '900px', height: '720px', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>Flex direction row
+          
+          <div style={{alignItems: 'center', justifyContent: 'space-evenly', width: '900px', height: '700px', display: 'flex', flexDirection: 'column'}}>
+
+            <div style={{justifyContent: 'space-evenly', width: '900px', display: 'flex', flexDirection: 'row'}}>
+
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>1</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>2</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>3</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>4</div>
+            
+            </div>
+
+            <div style={{justifyContent: 'space-evenly', width: '900px', display: 'flex', flexDirection: 'row'}}>
+
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>5</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>6</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>7</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>8</div>
+
+            </div>
+
+            <div style={{justifyContent: 'space-evenly', width: '900px', display: 'flex', flexDirection: 'row'}}>
+
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>9</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>10</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>11</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>12</div>
+
+            </div>
+
+          </div>  
+
+        </div>
+
+      
+
+        <div style={{fontWeight: 'bold', fontSize: '30px', display: 'flex', width: '900px', height: '720px', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>Flex direction row-reverse
+          
+          <div style={{alignItems: 'center', justifyContent: 'space-evenly', width: '900px', height: '700px', display: 'flex', flexDirection: 'column'}}>
+
+            <div style={{justifyContent: 'space-evenly', width: '900px', display: 'flex', flexDirection: 'row-reverse'}}>
+
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>1</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>2</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>3</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>4</div>
+            
+            </div>
+
+            <div style={{justifyContent: 'space-evenly', width: '900px', display: 'flex', flexDirection: 'row-reverse'}}>
+
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>5</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>6</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>7</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>8</div>
+
+            </div>
+
+            <div style={{justifyContent: 'space-evenly', width: '900px', display: 'flex', flexDirection: 'row-reverse'}}>
+
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>9</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>10</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>11</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>12</div>
+
+            </div>
+
+          </div>  
+
+        </div>
+
+      </div>
+
+      {/* Bloco inferior das DIVs : "Flex direction column" e "Flex direction column-reverse" */}
+      <div style={{width: '100vw', height: '930px', display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+
+        <div style={{fontWeight: 'bold', fontSize: '30px', display: 'flex', width: '700px', height: '930px', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>Flex direction column
+          
+          <div style={{alignItems: 'center', justifyContent: 'space-evenly', width: '700px', height: '900px', display: 'flex', flexDirection: 'row'}}>
+
+            <div style={{justifyContent: 'space-evenly', width: '200px', height: '900px', display: 'flex', flexDirection: 'column'}}>
+
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>1</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>2</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>3</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>4</div>
+            
+            </div>
+
+            <div style={{justifyContent: 'space-evenly', width: '200px', height: '900px', display: 'flex', flexDirection: 'column'}}>
+
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>5</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>6</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>7</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>8</div>
+
+            </div>
+
+            <div style={{justifyContent: 'space-evenly', width: '200px', height: '900px', display: 'flex', flexDirection: 'column'}}>
+
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>9</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>10</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>11</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>12</div>
+
+            </div>
+
+          </div>  
+
+        </div>
+
+        <div style={{fontWeight: 'bold', fontSize: '30px', display: 'flex', width: '700px', height: '930px', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>Flex direction column-reverse
+          
+          <div style={{alignItems: 'center', justifyContent: 'space-evenly', width: '700px', height: '900px', display: 'flex', flexDirection: 'row'}}>
+
+            <div style={{justifyContent: 'space-evenly', width: '200px', height: '900px', display: 'flex', flexDirection: 'column-reverse'}}>
+
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>1</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>2</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>3</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>4</div>
+            
+            </div>
+
+            <div style={{justifyContent: 'space-evenly', width: '200px', height: '900px', display: 'flex', flexDirection: 'column-reverse'}}>
+
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>5</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>6</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>7</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>8</div>
+
+            </div>
+
+            <div style={{justifyContent: 'space-evenly', width: '200px', height: '900px', display: 'flex', flexDirection: 'column-reverse'}}>
+
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>9</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>10</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>11</div>
+            <div style={{fontSize: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '200px', height: '200px', backgroundColor: 'red'}}>12</div>
+
+            </div>
+
+          </div>  
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
